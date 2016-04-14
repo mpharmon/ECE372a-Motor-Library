503c5420cea5bb3c640ba2c5db2cd7c1a7354df4
Mike <Mike@auth-10-3-1-222.wifi.mphnetinc.net>
2016-04-13 19:29:32 -0700

    Initial Commit

---
+++ Motor.c
@@ -0,0 +1,99 @@
+/*  
+ *  Class: ECE372a
+ * 
+ *  Group: 209
+ *
+ *  Lab: 3 Part 2
+ * 
+ *  File: PWM.c
+ * 
+ *  Member: Michael Harmon (Software)
+ *          Abdul Rana (Hardware)
+ *          James Beulke (System Integration)
+ *          Ali Hassan (Quality Control)
+ */
+
+#include "Motor.h"
+
+void Motor_Init(){
+#ifdef MOTOR1A_ANSEL
+  MOTOR1A_ANSEL = 0;
+#endif
+#ifdef MOTOR2A_ANSEL
+  MOTOR2A_ANSEL = 0;
+#endif
+#ifdef MOTOR1B_ANSEL
+  MOTOR1B_ANSEL = 0;
+#endif
+#ifdef MOTOR2B_ANSEL
+  MOTOR2B_ANSEL = 0;
+#endif
+  TRISDbits.TRISD5 = 0;
+  ODCDbits.ODCD5 = 1;
+  LATDbits.LATD5 = 1;
+  TRISDbits.TRISD8 = 0;
+  ODCDbits.ODCD8 = 1;
+  LATDbits.LATD8 = 1;
+  TMR2 = 0; // Set Timer 2 to Zero
+  PR2 = 10230; // Set Period
+  T2CONbits.TCKPS = 0; // Set Prescale of 1
+  OC1CONbits.OCM = 6; // PWM Mode on OC1, Fault Pin Disabled
+  OC2CONbits.OCM = 6; // PWM Mode on OC2, Fault Pin Disabled
+  OC1CONbits.OCTSEL = 0; // Use Timer 2
+  OC2CONbits.OCTSEL = 0; // Use Timer 2
+  RPD0Rbits.RPD0R = 0b1100; // OC1 Mapped to RD0
+  RPD1Rbits.RPD1R = 0b1011; // OC2 Mapped to RD1
+  OC1RS = 0; // Set Initial Values of OC1 to 0% Duty Cycle
+  OC2RS = 0; // Set Initial Values of OC2 to 0% Duty Cycle
+  T2CONbits.ON = 1; // Enable Timer 2
+  OC1CONbits.ON = 1; // Enable Output Compare 1
+  OC2CONbits.ON = 1; // Enable Output Compare 2
+}
+
+void Motor_Set1DutyCycle(float newDutyCycle){
+  if(newDutyCycle >= 100){
+    OC1RS = 10230;
+  }else{
+    OC1RS = floor(newDutyCycle * 102.3);
+  }
+}
+
+void Motor_Set2DutyCycle(float newDutyCycle){
+  if(newDutyCycle >= 100){
+    OC2RS = 10230;
+  }else{
+    OC2RS = floor(newDutyCycle * 102.3);
+  }
+}
+
+void Motor_Set1Forward(){
+  MOTOR1A_RP = 0;
+  MOTOR1B_RP = OC1_OUTPUT;
+  MOTOR1A_TRIS = TRIS_OUTPUT;
+  MOTOR1A_ODC = ODC_ENABLE;
+  MOTOR1A_LAT = LAT_ENABLE;
+}
+
+void Motor_Set2Forward(){
+  MOTOR2A_RP = 0;
+  MOTOR2B_RP = OC2_OUTPUT;
+  MOTOR2A_TRIS = TRIS_OUTPUT;
+  MOTOR2A_ODC = ODC_ENABLE;
+  MOTOR2A_LAT = LAT_ENABLE;
+}
+
+void Motor_Set1Backward(){
+  MOTOR1A_RP = OC1_OUTPUT;
+  MOTOR1B_RP = 0;
+  MOTOR1B_TRIS = TRIS_OUTPUT;
+  MOTOR1B_ODC = ODC_ENABLE;
+  MOTOR1B_LAT = LAT_ENABLE;
+}
+
+void Motor_Set2Backward(){
+  MOTOR2A_RP = OC2_OUTPUT;
+  MOTOR2B_RP = 0;
+  MOTOR2B_TRIS = TRIS_OUTPUT;
+  MOTOR2B_ODC = ODC_ENABLE;
+  MOTOR2B_LAT = LAT_ENABLE;
+}
---
+++ Motor.h
@@ -0,0 +1,84 @@
+/*  
+ *  Class: ECE372a
+ * 
+ *  Group: 209
+ * 
+ *  Motor Library
+ * 
+ *  File: Motor.h
+ * 
+ *  Member: Michael Harmon (Software)
+ *          Abdul Rana (Hardware)
+ *          James Beulke (System Integration)
+ *          Ali Hassan (Quality Control)
+ */
+
+#include <xc.h>
+#include <math.h>
+
+// Define Registers for Motor 1, A Input
+#define MOTOR1A_TRIS    TRISDbits.TRISD0
+#define MOTOR1A_ODC     ODCDbits.ODCD0
+#define MOTOR1A_LAT     LATDbits.LATD0
+#define MOTOR1A_RP      RPD0Rbits.RPD0R
+//#define MOTOR2B_ANSEL 
+
+
+// Define Registers for Motor 2, A Input
+#define MOTOR2A_TRIS    TRISDbits.TRISD1
+#define MOTOR2A_ODC     ODCDbits.ODCD1
+#define MOTOR2A_LAT     LATDbits.LATD1
+#define MOTOR2A_RP      RPD1Rbits.RPD1R
+#define MOTOR2A_ANSEL   ANSELDbits.ANSD1
+
+
+// Define Registers for Motor 2, B Input
+#define MOTOR2B_TRIS    TRISDbits.TRISD5
+#define MOTOR2B_ODC     ODCDbits.ODCD5
+#define MOTOR2B_LAT     LATDbits.LATD5
+#define MOTOR2B_RP      RPD5Rbits.RPD5R
+//#define MOTOR2B_ANSEL 
+
+
+// Define Registers for Motor 1, BInput
+#define MOTOR1B_TRIS    TRISDbits.TRISD8
+#define MOTOR1B_ODC     ODCDbits.ODCD8
+#define MOTOR1B_LAT     LATDbits.LATD8
+#define MOTOR1B_RP      RPD8Rbits.RPD8R
+//#define MOTOR1B_ANSEL 
+
+// Define OC1/OC2 Peripheral Select
+#define OC1_OUTPUT 0b1100
+#define OC2_OUTPUT 0b1011
+
+// Define Tristate Output
+#define TRIS_OUTPUT 0
+
+// Define ODC Statuses
+#define ODC_ENABLE 1
+#define ODC_DISABLE 0
+
+// Define Latch Statuses
+#define LAT_ENABLE 1
+#define LAT_DISABLE 0
+
+// Initializes the PWM (Defaults to Forward with 0% Duty Cycle)
+void Motor_Init();
+
+// Remaps Motor 1 Connections to Move the Motor Backward
+void Motor_Set1Backward();
+
+// Changes the Duty Cycle for Motor 1 (takes 0-100.0)
+void Motor_Set1DutyCycle(float newDutyCycle);
+
+// Remaps Motor 1 Connections to Move the Motor Forward
+void Motor_Set1Forward();
+
+// Remaps Motor 2 Connections to Move the Motor Backward
+void Motor_Set2Backward();
+
+// Changes the Duty Cycle for Motor 2 (takes 0-100.0)
+void Motor_Set2DutyCycle(float newDutyCycle);
+
+// Remaps Motor 2 Connections to Move the Motor Forward
+void Motor_Set2Forward();
